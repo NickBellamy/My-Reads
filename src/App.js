56df2a62e4e06efdcc5ed799cd43f7bfd87ccae6
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-07-18 15:31:24 +0100

    feat: Save books to state from BooksAPI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI';
 import { Route } from 'react-router-dom';
 import RenderShelves from './RenderShelves';
 import SearchBooks from './SearchBooks';
 import './App.css';
 
 class BooksApp extends React.Component {
-  state = {};
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
 
   render() {
+    console.log(this.state.books);
     return (
       <div className="app">
         <Route exact path="/" component={RenderShelves} />
