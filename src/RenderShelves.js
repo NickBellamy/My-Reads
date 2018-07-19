ae05ec8485540c06be23b456a8435462d9e39df6
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-07-19 12:59:27 +0100

    refactor: Eliminate repetition in RenderShelves

--- src/RenderShelves.js
+++ src/RenderShelves.js
@@ -10,123 +10,48 @@ class RenderShelves extends Component {
         </div>
         <div className="list-books-content">
           <div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {this.props.books.currentlyReading.map(book => (
-                    <li key={book.title}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div
-                          className="book-cover"
-                          style={{
-                            width: 128,
-                            height: 193,
-                            backgroundImage:
-                              `url("${book.imageLinks.smallThumbnail}")`
-                          }}
-                        />
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>
-                              Move to...
-                            </option>
-                            <option value="currentlyReading">
-                              Currently Reading
-                            </option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
+            {Object.keys(this.props.books).map(shelf => (
+              <div className="bookshelf">
+                <h2 className="bookshelf-title">{shelf}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {this.props.books[shelf].map(book => (
+                      <li key={book.title}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div
+                              className="book-cover"
+                              style={{
+                                width: 128,
+                                height: 193,
+                                backgroundImage: `url("${
+                                  book.imageLinks.smallThumbnail
+                                }")`
+                              }}
+                            />
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="move" disabled>
+                                  Move to...
+                                </option>
+                                <option value="currentlyReading">
+                                  Currently Reading
+                                </option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors}</div>
                         </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                    </div>
-                  </li>
-                  ))}
-                </ol>
+                      </li>
+                    ))}
+                  </ol>
+                </div>
               </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                {this.props.books.wantToRead.map(book => (
-                    <li key={book.title}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div
-                          className="book-cover"
-                          style={{
-                            width: 128,
-                            height: 193,
-                            backgroundImage:
-                              `url("${book.imageLinks.smallThumbnail}")`
-                          }}
-                        />
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>
-                              Move to...
-                            </option>
-                            <option value="currentlyReading">
-                              Currently Reading
-                            </option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                    </div>
-                  </li>
-                  ))}
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                {this.props.books.read.map(book => (
-                    <li key={book.title}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div
-                          className="book-cover"
-                          style={{
-                            width: 128,
-                            height: 193,
-                            backgroundImage:
-                              `url("${book.imageLinks.smallThumbnail}")`
-                          }}
-                        />
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>
-                              Move to...
-                            </option>
-                            <option value="currentlyReading">
-                              Currently Reading
-                            </option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{book.title}</div>
-                      <div className="book-authors">{book.authors}</div>
-                    </div>
-                  </li>
-                  ))}
-                </ol>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
         <div className="open-search">
