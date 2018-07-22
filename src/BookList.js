bf7c60f8cd890c2c613ee45084f8ea5026929623
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-07-22 13:43:30 +0100

    refactor: Convert BookList to a FSC

--- src/BookList.js
+++ src/BookList.js
@@ -2,55 +2,56 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { convertFromCamel } from './helpers';
 
-class BookList extends React.Component {
-static propTypes = {
-    books: PropTypes.array.isRequired,
-    shelves: PropTypes.array.isRequired,
-    moveBook: PropTypes.func.isRequired
-}
-  render() {
-    return (
-      <ol className="books-grid">
-        {this.props.books.map(book => (
-          <li key={book.industryIdentifiers[0].identifier}>
-            <div className="book">
-              <div className="book-top">
-                <div
-                  className="book-cover"
-                  style={{
-                    width: 128,
-                    height: 193,
-                    //TODO: Add "No image available"
-                    backgroundImage: `url("${book.imageLinks ? book.imageLinks.smallThumbnail : ''}")`
-                  }}
-                />
-                <div className="book-shelf-changer">
-                  <select
-                    defaultValue={book.shelf}
-                    onChange={event => {
-                      this.props.moveBook(book, event.target.value);
-                    }}
-                  >
-                    <option value="move" disabled>
-                      Move to...
-                    </option>
-                    {this.props.shelves.map(shelf => (
-                      <option key={shelf} value={shelf}>
-                        {convertFromCamel(shelf)}
-                      </option>
-                    ))}
-                    <option value="none">None</option>
-                  </select>
-                </div>
-              </div>
-              <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors ? book.authors : 'Unknown Author'}</div>
+const BookList = ({ books, shelves, moveBook }) => (
+  <ol className="books-grid">
+    {books.map(book => (
+      <li key={book.industryIdentifiers[0].identifier}>
+        <div className="book">
+          <div className="book-top">
+            <div
+              className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                //TODO: Add "No image available"
+                backgroundImage: `url("${
+                  book.imageLinks ? book.imageLinks.smallThumbnail : ''
+                }")`
+              }}
+            />
+            <div className="book-shelf-changer">
+              <select
+                defaultValue={book.shelf}
+                onChange={event => {
+                  moveBook(book, event.target.value);
+                }}
+              >
+                <option value="move" disabled>
+                  Move to...
+                </option>
+                {shelves.map(shelf => (
+                  <option key={shelf} value={shelf}>
+                    {convertFromCamel(shelf)}
+                  </option>
+                ))}
+                <option value="none">None</option>
+              </select>
             </div>
-          </li>
-        ))}
-      </ol>
-    );
-  }
-}
+          </div>
+          <div className="book-title">{book.title}</div>
+          <div className="book-authors">
+            {book.authors ? book.authors : 'Unknown Author'}
+          </div>
+        </div>
+      </li>
+    ))}
+  </ol>
+);
+
+BookList.propTypes = {
+  books: PropTypes.array.isRequired,
+  shelves: PropTypes.array.isRequired,
+  moveBook: PropTypes.func.isRequired
+};
 
 export default BookList;
