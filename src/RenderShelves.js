dab904463317cc81457081f5385110c490315c8e
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-07-22 13:38:06 +0100

    refactor: Convert RenderShelves to a FSC

--- src/RenderShelves.js
+++ src/RenderShelves.js
@@ -1,43 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import BookList from './BookList';
 import { convertFromCamel } from './helpers';
 
-class RenderShelves extends Component {
-  static propTypes = {
-    books: PropTypes.object.isRequired,
-    moveBook: PropTypes.func.isRequired
-  };
-
-  render() {
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {Object.keys(this.props.books).map(shelf => (
-              <div className="bookshelf" key={shelf}>
-                <h2 className="bookshelf-title">{convertFromCamel(shelf)}</h2>
-                <div className="bookshelf-books">
-                  <BookList
-                    books={this.props.books[shelf]}
-                    shelves={Object.keys(this.props.books)}
-                    moveBook={this.props.moveBook}
-                  />
-                </div>
-              </div>
-            ))}
+const RenderShelves = ({ books, moveBook }) => (
+  <div className="list-books">
+    <div className="list-books-title">
+      <h1>MyReads</h1>
+    </div>
+    <div className="list-books-content">
+      <div>
+        {Object.keys(books).map(shelf => (
+          <div className="bookshelf" key={shelf}>
+            <h2 className="bookshelf-title">{convertFromCamel(shelf)}</h2>
+            <div className="bookshelf-books">
+              <BookList
+                books={books[shelf]}
+                shelves={Object.keys(books)}
+                moveBook={moveBook}
+              />
+            </div>
           </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+    <div className="open-search">
+      <Link to="/search">Add a book</Link>
+    </div>
+  </div>
+);
+
+RenderShelves.propTypes = {
+  books: PropTypes.object.isRequired,
+  moveBook: PropTypes.func.isRequired
+};
 
 export default RenderShelves;
